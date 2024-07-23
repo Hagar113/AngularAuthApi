be959944591e3d98029d2893eb23747f31530747
elhaw <elhaw@HAJER>
2024-07-23 21:53:11 +0300

    commit

---
+++ AngularAuthApi/Localization/LocalizationMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AngularAuthApi.Middlewares;
+
+namespace AngularAuthApi.Localization
+{
+    public static class LocalizationMiddlewareExtensions
+    {
+
+        public static IApplicationBuilder UseLocalization(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LocalizationMiddleware>();
+        }
+    }
+}
